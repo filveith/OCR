32cb273d7a0216e6be3ed5f58e213bcef4a008cf
filveith <fil.veith@gmx.com>
2022-03-14 18:05:07 +0100

    zoning fonctionne

--- mainFil.py
+++ mainFil.py
@@ -1,3 +1,4 @@
+from ast import boolop
 from traceback import print_tb
 from PIL import Image, ImageOps
 from os import walk
@@ -8,15 +9,15 @@ from re import A, sub
 
 def unBlur(img, name):
     path = './clean/'+name+''
+    img = img.resize((50, 50))
     imagePixels = list(img.getdata())
     newImage = []
+    
     for p in imagePixels:
         if p >= 255/2:
             newImage.append(255)
         else:
             newImage.append(0)       
-
-    print(newImage)
 
     im2 = Image.new(img.mode, img.size)
     im2.putdata(newImage)
@@ -56,11 +57,38 @@ def getBinaryImg(img):
     # print(binaryImage)
     return binaryImage
 
-def zoning(img, nb_zones = 4):
-    refImg = './projetOCR/Reference/1.png'
-    ukwImg = './ProjetOCR/clean/1_1.png'
+def avgZone(zone):
+    size = len(zone)
+    sum = 0
+    for p in zone:
+        sum += int(not bool(p))
+
+    avg = sum / size * 100
+    print(avg)
     
+    return avg
+
+def getReference():
     return 0
+
+def zoning(ukwImg, nb_zones = 4):
+
+    binImg = getBinaryImg(ukwImg)
+    
+    # for i in range(nb_zones):
+        
+    zone1 = [binImg[x][y] for x in range(0,25) for y in range(0,25)]
+    zone2 = [binImg[y][x] for x in range(25,50) for y in range(0,25)]
+    zone3 = [binImg[y][x] for x in range(0,25) for y in range(25,50)]
+    zone4 = [binImg[y][x] for x in range(25,50) for y in range(25,50)]
+
+    avgZone(zone1)
+    avgZone(zone2)
+    avgZone(zone3)
+    avgZone(zone4)
+
+    return 0
+
 
 def dilatation(img, dilater_size=3):
     imagePixels = list(img.getdata())
@@ -95,7 +123,7 @@ def getsubgrid(origin, size, grid):
 def erosion_is_ok(eroder, to_erode):
     flat_eroder = list(chain.from_iterable(eroder))
     flat_to_erode = list(chain.from_iterable(to_erode))
-    print(flat_eroder, flat_to_erode)
+    # print(flat_eroder, flat_to_erode)
     print()
     for i in range(0, len(flat_eroder)):
         if flat_eroder[i] == 1 and flat_eroder[i] != flat_to_erode[i]:          
@@ -104,7 +132,8 @@ def erosion_is_ok(eroder, to_erode):
         
 
 def erosion(bin_img, imageName, eroder=[[1 for x in range(3)] for y in range(3)]):
-    print(imageName, bin_img)
+    print(imageName)
+    print(bin_img)
     height, width = len(bin_img), len(bin_img[0])
     print(width, height)
     eroder_size = len(eroder)
@@ -112,16 +141,15 @@ def erosion(bin_img, imageName, eroder=[[1 for x in range(3)] for y in range(3)]
     output=np.array([[0 for x in range(width)] for y in range(height)])
     for i in range(offset,height-offset):
         for j in range(offset, width-offset):
-            subarray = getsubgrid((i,j),eroder_size,a)
+            subarray = getsubgrid((i,j), eroder_size, bin_img)
+            print(subarray)
             output[i,j] = int(erosion_is_ok(eroder, subarray))
 
-    createImgFromBin(output, imageName)
+    # createImgFromBin(output, imageName)
 
 
 # with ImageOps.grayscale(Image.open("./out/dilatation.png")) as img:
 #     dilatation(img) 
-
-
 
 a = [[0, 0, 0, 0, 0, 0, 0],
      [0, 0, 1, 1, 1, 0, 0],
@@ -130,8 +158,6 @@ a = [[0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 1, 1, 0, 0],
      [0, 0, 1, 1, 1, 0, 0],
      [0, 0, 0, 0, 0, 0, 0]]    
-
-
 
 # createImgFromBin(a, 'dilatation')
 
@@ -150,8 +176,14 @@ for image in images:
 
 cleanImages = getCleanImages()
 
-for image in cleanImages:
-    if image != '.DS_Store':
-        with Image.open("./clean/"+image) as img:
-            binImg = getBinaryImg(img)
-            erosion(binImg, image)
+# for image in cleanImages:
+#     if image != '.DS_Store':
+#         with Image.open("./clean/"+image) as img:
+#             binImg = getBinaryImg(img)
+#             erosion(binImg, image)
+
+print("img1")
+zoning(Image.open("./clean/1_1.png"))
+print("img2")
+zoning(Image.open("./clean/1_5.png"))
+
