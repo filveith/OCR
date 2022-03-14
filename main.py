3b936cc24db41af66d1dd0ab2f6e2319424b50a1
filveith <fil.veith@gmx.com>
2022-03-14 18:11:31 +0100

    merge

--- main.py
+++ main.py
@@ -10,21 +10,45 @@ TRAIN = "train/"
 TEST = "test/"
 
 
-def avgEachPixel(img, name):
+def standardize(img, name):
     path = './clean/'+name+''
+    img = img.resize((50, 50))
     imagePixels = list(img.getdata())
     newImage = []
+    
     for p in imagePixels:
-        avg = (p[0]+p[1]+p[2])/3
-        if avg >= 255/2:
-            newImage.append((255,255,255))
+        if p >= 255/2:
+            newImage.append(255)
         else:
-            newImage.append((0,0,0))       
+            newImage.append(0)       
 
     im2 = Image.new(img.mode, img.size)
     im2.putdata(newImage)
     im2.save(path)
+    return im2
     
+
+def getBinaryImg(img):
+    imagePixels = list(img.getdata())
+    binaryImage = []
+    width, height = img.size
+    nb = 0
+    row = []
+    for p in imagePixels:
+        nb += 1
+        if p == 255:
+            row.append(1)
+        else:
+            row.append(0)   
+
+        if width == nb:
+            binaryImage.append(row)
+            nb = 0
+            row = []
+
+    # print(binaryImage)
+    return binaryImage
+
 def getImages(set):
     imagesNames = next(walk(set), (None, None, []))[2]
     results = []
@@ -44,10 +68,17 @@ def getLabels(set):
     return results
 
 def createImgFromBin(img_2d_list, filename):
-    flat = list(chain.from_iterable(img_2d_list))
-    im2 = Image.new('1', (len(img_2d_list), len(img_2d_list[0])))
+    flat = list(img_2d_list)
+    width, height = img_2d_list.size
+    # Check if list is nested or not (if yes we flaten it)
+    if any(isinstance(i, list) for i in img_2d_list): 
+        flat = list(chain.from_iterable(img_2d_list))
+        width = len(img_2d_list[0])
+        height = len(img_2d_list)
+
+    im2 = Image.new('1', (height, width))
     im2.putdata(flat)
-    im2.save('./out/'+filename+'.png')
+    im2.save('./clean/'+filename)
 
 def getsubgrid(origin, size, grid):
     offset = floor(size/2)
@@ -71,11 +102,35 @@ def erosion(bin_img, eroder=[[1 for x in range(3)] for y in range(3)]):
     output=np.array([[0 for x in range(width)] for y in range(height)])
     for i in range(offset,height-offset):
         for j in range(offset, width-offset):
-            subarray = getsubgrid((i,j),eroder_size,a)
+            #Fix this getSubGrid(bin_img)
+            subarray = getsubgrid((i,j),eroder_size, bin_img)
             output[i,j] = int(erosion_is_ok(eroder, subarray))
                   
     createImgFromBin(output, "retest")
 
+def dilatation(img, dilater_size=3):
+    imagePixels = list(img.getdata())
+    newImage = imagePixels
+
+    width, height = img.size
+    print(imagePixels)
+
+    for i, p in enumerate(imagePixels):
+        print(p)
+        if p == 255:
+            for x in range(width*-1,width*2,width):
+                for y in range(dilater_size):
+                    try:
+                        print("x: ",x," y: ",y," i: ",i,"  co:",(i+x+y)," value: ",imagePixels[(i+x)+y])
+                        if imagePixels[(i+x)+y] == 0:
+                            print("255")
+                            newImage[(i+x)+y] = 255
+                    except:
+                        pass
+
+    print(newImage)
+    createImgFromBin(newImage, 'res_dilatation', img.size[0], img.size[1])
+    return newImage
 
 # def knn(training_set, training_labels, testing_set, k = 5):
 #     predictions = []
@@ -90,7 +145,34 @@ def getStatsOfImage(img):
     
     return stats
 
+def zoning(ukwImg, nb_zones = 4):
+    
+    binImg = getBinaryImg(ukwImg)
+    
+    # for i in range(nb_zones):
+        
+    zone1 = [binImg[x][y] for x in range(0,25) for y in range(0,25)]
+    zone2 = [binImg[y][x] for x in range(25,50) for y in range(0,25)]
+    zone3 = [binImg[y][x] for x in range(0,25) for y in range(25,50)]
+    zone4 = [binImg[y][x] for x in range(25,50) for y in range(25,50)]
 
+    avgZone(zone1)
+    avgZone(zone2)
+    avgZone(zone3)
+    avgZone(zone4)
+
+    return 0
+
+def avgZone(zone):
+    size = len(zone)
+    sum = 0
+    for p in zone:
+        sum += int(not bool(p))
+
+    avg = sum / size * 100
+    print(avg)
+    
+    return avg
 
 def distanceEuclidienne(vecteur1, vecteur2):
     """Calcule la distance euclidienne entre deux vecteurs
--- mainFil.py
+++ mainFil.py
@@ -7,112 +7,110 @@ from math import floor
 import numpy as np
 from re import A, sub
 
-def unBlur(img, name):
-    path = './clean/'+name+''
-    img = img.resize((50, 50))
-    imagePixels = list(img.getdata())
-    newImage = []
+# def unBlur(img, name):
+#     path = './clean/'+name+''
+#     img = img.resize((50, 50))
+#     imagePixels = list(img.getdata())
+#     newImage = []
     
-    for p in imagePixels:
-        if p >= 255/2:
-            newImage.append(255)
-        else:
-            newImage.append(0)       
+#     for p in imagePixels:
+#         if p >= 255/2:
+#             newImage.append(255)
+#         else:
+#             newImage.append(0)       
 
-    im2 = Image.new(img.mode, img.size)
-    im2.putdata(newImage)
-    im2.save(path)
-    return im2
+#     im2 = Image.new(img.mode, img.size)
+#     im2.putdata(newImage)
+#     im2.save(path)
+#     return im2
     
-def createImgFromBin(img_2d_list, filename, width=10, height=10):
-    flat = list(img_2d_list)
-    # Check if list is nested or not (if yes we flaten it)
-    if any(isinstance(i, list) for i in img_2d_list): 
-        flat = list(chain.from_iterable(img_2d_list))
-        width = len(img_2d_list[0])
-        height = len(img_2d_list)
+# def createImgFromBin(img_2d_list, filename, width=10, height=10):
+#     flat = list(img_2d_list)
+#     # Check if list is nested or not (if yes we flaten it)
+#     if any(isinstance(i, list) for i in img_2d_list): 
+#         flat = list(chain.from_iterable(img_2d_list))
+#         width = len(img_2d_list[0])
+#         height = len(img_2d_list)
 
-    im2 = Image.new('1', (height, width))
-    im2.putdata(flat)
-    im2.save('./clean/'+filename)
+#     im2 = Image.new('1', (height, width))
+#     im2.putdata(flat)
+#     im2.save('./clean/'+filename)
 
-def getBinaryImg(img):
-    imagePixels = list(img.getdata())
-    binaryImage = []
-    width, height = img.size
-    nb = 0
-    row = []
-    for p in imagePixels:
-        nb += 1
-        if p == 255:
-            row.append(1)
-        else:
-            row.append(0)   
+# def getBinaryImg(img):
+#     imagePixels = list(img.getdata())
+#     binaryImage = []
+#     width, height = img.size
+#     nb = 0
+#     row = []
+#     for p in imagePixels:
+#         nb += 1
+#         if p == 255:
+#             row.append(1)
+#         else:
+#             row.append(0)   
 
-        if width == nb:
-            binaryImage.append(row)
-            nb = 0
-            row = []
+#         if width == nb:
+#             binaryImage.append(row)
+#             nb = 0
+#             row = []
 
-    # print(binaryImage)
-    return binaryImage
+#     # print(binaryImage)
+#     return binaryImage
 
-def avgZone(zone):
-    size = len(zone)
-    sum = 0
-    for p in zone:
-        sum += int(not bool(p))
+# def avgZone(zone):
+#     size = len(zone)
+#     sum = 0
+#     for p in zone:
+#         sum += int(not bool(p))
 
-    avg = sum / size * 100
-    print(avg)
+#     avg = sum / size * 100
+#     print(avg)
     
-    return avg
+#     return avg
 
-def getReference():
-    return 0
 
-def zoning(ukwImg, nb_zones = 4):
+# def zoning(ukwImg, nb_zones = 4):
 
-    binImg = getBinaryImg(ukwImg)
+#     binImg = getBinaryImg(ukwImg)
     
-    # for i in range(nb_zones):
+#     # for i in range(nb_zones):
         
-    zone1 = [binImg[x][y] for x in range(0,25) for y in range(0,25)]
-    zone2 = [binImg[y][x] for x in range(25,50) for y in range(0,25)]
-    zone3 = [binImg[y][x] for x in range(0,25) for y in range(25,50)]
-    zone4 = [binImg[y][x] for x in range(25,50) for y in range(25,50)]
+#     zone1 = [binImg[x][y] for x in range(0,25) for y in range(0,25)]
+#     zone2 = [binImg[y][x] for x in range(25,50) for y in range(0,25)]
+#     zone3 = [binImg[y][x] for x in range(0,25) for y in range(25,50)]
+#     zone4 = [binImg[y][x] for x in range(25,50) for y in range(25,50)]
 
-    avgZone(zone1)
-    avgZone(zone2)
-    avgZone(zone3)
-    avgZone(zone4)
+#     avgZone(zone1)
+#     avgZone(zone2)
+#     avgZone(zone3)
+#     avgZone(zone4)
 
-    return 0
+#     return 0
 
 
-def dilatation(img, dilater_size=3):
-    imagePixels = list(img.getdata())
-    newImage = imagePixels
+# def dilatation(img, dilater_size=3):
+#     imagePixels = list(img.getdata())
+#     newImage = imagePixels
 
-    width, height = img.size
-    print(imagePixels)
+#     width, height = img.size
+#     print(imagePixels)
 
-    for i, p in enumerate(imagePixels):
-        print(p)
-        if p == 255:
-            for x in range(width*-1,width*2,width):
-                for y in range(dilater_size):
-                    try:
-                        print("x: ",x," y: ",y," i: ",i,"  co:",(i+x+y)," value: ",imagePixels[(i+x)+y])
-                        if imagePixels[(i+x)+y] == 0:
-                            print("255")
-                            newImage[(i+x)+y] = 255
-                    except:
-                        pass
+#     for i, p in enumerate(imagePixels):
+#         print(p)
+#         if p == 255:
+#             for x in range(width*-1,width*2,width):
+#                 for y in range(dilater_size):
+#                     try:
+#                         print("x: ",x," y: ",y," i: ",i,"  co:",(i+x+y)," value: ",imagePixels[(i+x)+y])
+#                         if imagePixels[(i+x)+y] == 0:
+#                             print("255")
+#                             newImage[(i+x)+y] = 255
+#                     except:
+#                         pass
 
-    print(newImage)
-    createImgFromBin(newImage, 'res_dilatation', img.size[0], img.size[1])
-    return newImage
+#     print(newImage)
+#     createImgFromBin(newImage, 'res_dilatation', img.size[0], img.size[1])
+#     return newImage
 
 def getsubgrid(origin, size, grid):
     offset = floor(size/2)
